8ad6504ef485df9cdf8dadc20a58959ab034d28a
Benanna2019 <bass41992ben@gmail.com>
2020-09-30 13:01:44 -0400

    began simple project using reach/router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link, Router } from "@reach/router";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Tutorial</h1>
+        <nav>
+          <Link to="/">Home</Link> <Link to="dashboard">Dashboard</Link>{" "}
+          <Link to="invoices">Invoices</Link>
+        </nav>
+        <Router>
+          <Home path="/" />
+          <Dashboard path="/dashboard" />
+          <Invoices path="invoices">
+            <Invoice path=":invoiceId" />
+          </Invoices>
+        </Router>
       </header>
     </div>
   );
 }
+
+const Home = () => (
+  <div>
+    <h2>Welcome</h2>
+  </div>
+);
+
+const Dashboard = () => (
+  <div>
+    <h2>Dashboard</h2>
+  </div>
+);
+
+const Invoice = (props) => (
+  <div>
+    <h2>Invoice {props.invoiceId}</h2>
+  </div>
+);
+
+const Invoices = (props) => (
+  <div>
+    <h2>Invoices</h2>
+    <ul>
+      <li>
+        <Link to="123">Invoice 123</Link>
+      </li>
+      <li>
+        <Link to="abc">Invoice ABC</Link>
+      </li>
+    </ul>
+
+    {props.children}
+  </div>
+);
 
 export default App;
